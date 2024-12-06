7d4d64fbd42ffcfd8fe3007a3b11779410718ff1
Kevin Dichter <kevinidichter@gmail.com>
2024-12-05 23:43:46 -0500

    taser disabled demon enemy

--- Game3/Assets/Scripts/TaserScript.cs
+++ Game3/Assets/Scripts/TaserScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TaserScript : MonoBehaviour
 {
     //public float range = 100f;
+    [SerializeField] public AudioClip taserSound;
     public Camera fpsCam;
     public ObjectGrabRelease script;
     private bool canShoot = true;
@@ -16,15 +17,17 @@ public class TaserScript : MonoBehaviour
             if (canShoot && Input.GetButtonDown("Fire1") && script.objectInHand.name == "Taser")
             {
                 Shoot();
+                SoundFXManager.instance.PlaySoundFXClip(taserSound, transform, 0.5f);
                 canShoot = false;
             }
         }
         if (!canShoot)
         {
             timer += Time.deltaTime;
-            if (timer >= 30)
+            if (timer >= 3)
             {
                 canShoot = true;
+                timer = 0;
             }
         }
     }
@@ -38,7 +41,7 @@ public class TaserScript : MonoBehaviour
             Debug.Log(hit.transform.name);
             //hit.collider.gameObject.SetActive(false);
 
-            if (hit.collider.gameObject.name == "Enemy")
+            if (hit.collider.gameObject.tag == "Enemy")
             {
 
                 hit.collider.gameObject.SetActive(false); // Set the object to active
