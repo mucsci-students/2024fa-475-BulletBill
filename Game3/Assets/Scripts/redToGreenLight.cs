d54996ffef564e31b636b58c4825f3db8df2eef7
Kevin Dichter <kevinidichter@gmail.com>
2024-12-05 19:38:51 -0500

    added the light toggle script

---
+++ Game3/Assets/Scripts/redToGreenLight.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class redToGreenLight : MonoBehaviour
+{
+    public ObjectGrabRelease script;
+    public GameObject hiddenLever;
+    public GameObject basementRedLight;
+    public GameObject basementGreenLight;
+    public GameObject exitRedLight;
+    public GameObject exitGreenLight;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (script.objectInHand.name == "Lever")
+        {
+            hiddenLever.SetActive(true);
+            basementRedLight.SetActive(false);
+            exitRedLight.SetActive(false);
+            basementGreenLight.SetActive(true);
+            exitGreenLight.SetActive(true);
+        }
+    }
+}
