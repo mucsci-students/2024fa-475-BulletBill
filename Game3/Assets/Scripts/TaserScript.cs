c4873841c4f117ad2a99fcfdd1c7787d3bd882d8
Kevin Dichter <kevinidichter@gmail.com>
2024-12-07 17:38:17 -0500

    added taser cooldown

--- Game3/Assets/Scripts/TaserScript.cs
+++ Game3/Assets/Scripts/TaserScript.cs
@@ -7,10 +7,38 @@ public class TaserScript : MonoBehaviour
 {
     //public float range = 100f;
     [SerializeField] public AudioClip taserSound;
+    [SerializeField] public float coolDownTime = 30f;
     public Camera fpsCam;
     public ObjectGrabRelease script;
     private bool canShoot = true;
-    private float timer = 0f;
+    private float timer;
+    private GUIStyle taserStyle;
+
+    void Start()
+    {
+        timer = coolDownTime;
+
+        // Initialize the style
+        taserStyle = new GUIStyle();
+
+        // Set the font style to bold
+        taserStyle.fontStyle = FontStyle.Bold;
+
+        // Set the text color to white
+        taserStyle.normal.textColor = Color.white;
+
+        // Create a black texture and assign it as the button's background
+        Texture2D blackTexture = new Texture2D(1, 1);
+        blackTexture.SetPixel(0, 0, Color.black);
+        taserStyle.normal.background = blackTexture;
+
+        // Adjust font size if needed
+        taserStyle.fontSize = 75;
+
+        // Adjust padding or other properties as needed
+        taserStyle.padding = new RectOffset(10, 10, 10, 10);
+    }
+
     void Update()
     {
         if (script.objectInHand != null)
@@ -24,11 +52,11 @@ public class TaserScript : MonoBehaviour
         }
         if (!canShoot)
         {
-            timer += Time.deltaTime;
-            if (timer >= 3)
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
             {
                 canShoot = true;
-                timer = 0;
+                timer = coolDownTime;
             }
         }
     }
@@ -51,5 +79,19 @@ public class TaserScript : MonoBehaviour
             }
             
         }
+    }
+    void OnGUI()
+    {
+        if (script.objectInHand != null && script.objectInHand.name == "Taser")
+        {
+            if (timer < 30f)
+            {
+                GUI.Box(new Rect(10, 300, 1250, 100), "Taser Cooldwon: " + timer.ToString("F2"), taserStyle);
+            }
+            else
+            {
+                GUI.Box(new Rect(10, 300, 1250, 100), "Taser Ready!", taserStyle);
+            }
+        }   
     }
 }
